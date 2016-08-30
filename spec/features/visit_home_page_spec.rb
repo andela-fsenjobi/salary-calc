8152e6a2ef3512ae6abae96573844cad78a5d71f
Femi Senjobi <femi.senjobi@andela.com>
2016-08-30 17:47:22 +0100

    set up smoke test

---
+++ spec/features/visit_home_page_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe WelcomeController, type: :feature do
+  it "navigates to the welcome page" do
+    visit root_path
+    expect(page).to have_content "Welcome"
+  end
+end
